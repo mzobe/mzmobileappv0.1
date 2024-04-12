f8b28ae8fab94670e8642a2031199d031016c873
roboguru <mzobeinc@gmail.com>
2024-04-12 20:04:54 +0300

    initial

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="flex-1 items-center align-center bg-green-500">
+      <Text className="text-red-600 text-4xl my-50 italic bold">Home page</Text>
+    </View>
+  );
+}
